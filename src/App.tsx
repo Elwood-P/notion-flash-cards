24a07c1f353553c288bed8890c10ac5a1aba5cb7
Elwood-P <info@pjlittlewood.co.uk>
2023-04-26 09:31:27 +0100

    Create CardBack and Overlay components

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
 import CardFront from './components/CardFront';
-import {fetchCards} from './store/cardsSlice'
+import CardBack from './components/CardBack';
+import Overlay from './components/Overlay';
+import { fetchCards } from './store/cardsSlice';
 
 function App() {
   const cards = useSelector((state) => state.cards);
   const dispatch = useDispatch();
-
+  const showBack = true;
+  
   useEffect(() => {
     dispatch(fetchCards());
   }, [dispatch]);
@@ -20,6 +23,11 @@ function App() {
         {cards.map((card) => (
           <CardFront key={card.id} front={card.properties.Name.title[0].plain_text} />
         ))}
+        {showBack && (
+          <Overlay>
+            <CardBack />
+          </Overlay>
+        )}
       </div>
     </div>
   );
